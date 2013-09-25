75b60527e87172d83ea9d6756a5ad7cf4dadcf67
Justin Myers <justin@justinmyers.net>
2013-09-25 16:55:07 -0400

    Made the init function look a bit more correct

--- dist/playfair.js
+++ dist/playfair.js
@@ -48,7 +48,17 @@ PF.utils.parseHash = function parseHash( hash ) {
 //  -=-=-=-=-=-=-=-=-=-=-=-=-=- INITIALIZATION =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 PF.init = function init( hash, chartId ) {
-  // TODO: Add this.
+  var hashOptions = PF.utils.parseHash( hash );
+
+  google.load( 'visualization', '1.0', {
+    'packages': [ 'corechart' ]
+  });
+
+  google.setOnLoadCallback(function() {
+    var chartElem = window.document.getElementById( chartId );
+
+    log( 'Loaded.' );  // TODO: Fill out the rest of this.
+  });
 };
 
 
