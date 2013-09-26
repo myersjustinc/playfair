07591a4b092ed775d1799f1fa9cc2444776375f1
Justin Myers <justin@justinmyers.net>
2013-09-26 11:59:10 -0400

    Arranged PF.opts source alphabetically

--- dist/playfair.js
+++ dist/playfair.js
@@ -29,29 +29,6 @@ PF.defaults.chs = '300x200';
 
 PF.opts = PF.opts || {};
 
-PF.opts.chs = function chs( rawChs ) {  // Chart size
-  var dimsFormat = /^(\d+)x(\d+)$/,
-    dimsMatch,
-    dimsParsed,
-    dimsSplit;
-
-  if ( rawChs == null ) {
-    PF.utils.logError( 'chs (size) not specified. Using default.' );
-    rawChs = PF.defaults.chs;
-  }
-
-  dimsMatch = dimsFormat.exec( rawChs );
-  if ( !dimsMatch ) {
-    PF.utils.logError( 'chs (size) formatted incorrectly. Using default.' );
-    dimsMatch = dimsFormat.exec( PF.defaults.chs );
-  }
-
-  return {
-    height: parseInt( dimsMatch[ 2 ], 10 ),
-    width: parseInt( dimsMatch[ 1 ], 10 )
-  };
-};
-
 PF.opts.chd = function chd( rawChd ) {  // Chart data
   var chdFormat = /^([tse]):(.+)$/,
     chdFormatMatch,
@@ -192,6 +169,29 @@ PF.opts.chd = function chd( rawChd ) {  // Chart data
   }
 
   return parsedData;
+};
+
+PF.opts.chs = function chs( rawChs ) {  // Chart size
+  var dimsFormat = /^(\d+)x(\d+)$/,
+    dimsMatch,
+    dimsParsed,
+    dimsSplit;
+
+  if ( rawChs == null ) {
+    PF.utils.logError( 'chs (size) not specified. Using default.' );
+    rawChs = PF.defaults.chs;
+  }
+
+  dimsMatch = dimsFormat.exec( rawChs );
+  if ( !dimsMatch ) {
+    PF.utils.logError( 'chs (size) formatted incorrectly. Using default.' );
+    dimsMatch = dimsFormat.exec( PF.defaults.chs );
+  }
+
+  return {
+    height: parseInt( dimsMatch[ 2 ], 10 ),
+    width: parseInt( dimsMatch[ 1 ], 10 )
+  };
 };
 
 //  -=-=-=-=-=-=-=-=-=-=-=-=-= UTILITY FUNCTIONS =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
--- src/PF/opts.js
+++ src/PF/opts.js
@@ -2,29 +2,6 @@
 
 PF.opts = PF.opts || {};
 
-PF.opts.chs = function chs( rawChs ) {  // Chart size
-  var dimsFormat = /^(\d+)x(\d+)$/,
-    dimsMatch,
-    dimsParsed,
-    dimsSplit;
-
-  if ( rawChs == null ) {
-    PF.utils.logError( 'chs (size) not specified. Using default.' );
-    rawChs = PF.defaults.chs;
-  }
-
-  dimsMatch = dimsFormat.exec( rawChs );
-  if ( !dimsMatch ) {
-    PF.utils.logError( 'chs (size) formatted incorrectly. Using default.' );
-    dimsMatch = dimsFormat.exec( PF.defaults.chs );
-  }
-
-  return {
-    height: parseInt( dimsMatch[ 2 ], 10 ),
-    width: parseInt( dimsMatch[ 1 ], 10 )
-  };
-};
-
 PF.opts.chd = function chd( rawChd ) {  // Chart data
   var chdFormat = /^([tse]):(.+)$/,
     chdFormatMatch,
@@ -165,4 +142,27 @@ PF.opts.chd = function chd( rawChd ) {  // Chart data
   }
 
   return parsedData;
+};
+
+PF.opts.chs = function chs( rawChs ) {  // Chart size
+  var dimsFormat = /^(\d+)x(\d+)$/,
+    dimsMatch,
+    dimsParsed,
+    dimsSplit;
+
+  if ( rawChs == null ) {
+    PF.utils.logError( 'chs (size) not specified. Using default.' );
+    rawChs = PF.defaults.chs;
+  }
+
+  dimsMatch = dimsFormat.exec( rawChs );
+  if ( !dimsMatch ) {
+    PF.utils.logError( 'chs (size) formatted incorrectly. Using default.' );
+    dimsMatch = dimsFormat.exec( PF.defaults.chs );
+  }
+
+  return {
+    height: parseInt( dimsMatch[ 2 ], 10 ),
+    width: parseInt( dimsMatch[ 1 ], 10 )
+  };
 };
