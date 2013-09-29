17505eec8e5a3af21fbd8ad8601f0c4c6d7eabdb
Justin Myers <justin@justinmyers.net>
2013-09-28 21:29:04 -0400

    Added table generation to init

--- dist/playfair.js
+++ dist/playfair.js
@@ -283,7 +283,9 @@ PF.init = function init( hash, chartId ) {
 
   google.setOnLoadCallback(function() {
     var chartElem = window.document.getElementById( chartId ),
-      dimensions = PF.opts.chs( hashOptions.chs );
+      dimensions = PF.opts.chs( hashOptions.chs ),
+      table = PF.utils.buildTable(
+        PF.opts.chdl( hashOptions.chdl || hashOptions.chl ), PF.opts.chd );
 
     log( 'Loaded.' );  // TODO: Fill out the rest of this.
   });
